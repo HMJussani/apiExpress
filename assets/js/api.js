21d8acd8f8893222e5914fbf292db7897d04b61f
HMJussani <hmjussani@gmail.com>
2020-03-12 13:03:18 -0300

    rodando

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,17 +4,25 @@ const bodyParser = require('body-parser');
 const fireBase = require('./db')
 const app = express();
 const porta = 8080;
-
+const db = "Mandic";
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.render('index.ejs');   
+    res.render('index.ejs');
+});
+app.get('/show', function (req, res) {
+    res.send(fireBase.readDbAll(db));
+    res.redirect('/');
+});
+app.get('/getUser', function (req, res) {
+    res.send(fireBase.readDb(db,req.body));
+    res.redirect('/');
 });
 
 app.post('/salva', (req, res) => {
-    fireBase(req.body);
+    fireBase.writeDb(db, req.body);
     res.redirect('/');
 
 })
--- assets/js/db.js
+++ assets/js/db.js
@@ -11,42 +11,80 @@ const firebaseConfig = {
     measurementId: "G-GKSWD7NJ7T"
 };
 
-
 fireBase.initializeApp(firebaseConfig);
+fireBase.database().ref().on('value', function (snapshot) {});
 
-module.exports = function salvaDb(dados) {
-    fireBase.database().ref('Mandic').push(dados);
+function dbExiste(db, title) {
+    var existe = false;
+    fireBase.database().ref(db).child(title).on('value', function (snapshot) {
+        existe = snapshot.exists();
+    });
+    return existe;
 }
 
-/*
-if (firebase.initializeApp(firebaseConfig)) {
+const writeDb = (db, dados) => {
+    return fireBase.database().ref(db).push(dados);
+}
 
-    firebase.database().ref(`Tutoriais`).on(`value`, function (snapshot) {
-        var data = [];
+exports.writeDb = writeDb;
+
+const updateDb = (db, dados) => {
+    if (dbExiste(db, title) === false) {
+        alert(` ${title} não encontrado no banco de dados.`);
+        return;
+    }
+    return fireBase.database().ref(db).set(dados);
+}
+
+exports.updateDb = updateDb;
+
+const readDb = (db, title) => {
+    if (dbExiste(db, title) === false) {
+        alert(` ${title} não encontrado no banco de dados.`);
+        return;
+    }
+    var data = [];
+    fireBase.database().ref(db).child(title).on('value', function (snapshot) {
         var readings = snapshot.val();
         if (readings) {
             var currentValue;
             for (var key in readings) {
-                currentValue = readings[key];
+                currentValue = readings[key]
                 data.push(currentValue);
             }
-            criarLinks(data);
         }
     });
-
-    function lerBanco(title) {       
-        limparDiv();
-        criarDivs(getData(title));
-        document.querySelector("#title").value = title;
-    }
-
-    function apagaTuto(title) {       
-            var aRemover = firebase.database().ref(`Tutoriais/${title}/`);
-            aRemover.remove(function (error) {
-                alert(`${title} removido do banco de dados.`);
-                window.location.reload();
-            });
-        
-    }
+    return data;
 }
-*/
+
+exports.readDb = readDb;
+
+const readDbAll = (db) => {
+    var data = [];
+    fireBase.database().ref(db).on('value', function (snapshot) {
+        var readings = snapshot.val();
+        if (readings) {
+            var currentValue;
+            for (var key in readings) {
+                currentValue = readings[key]
+                data.push(currentValue);
+            }
+        }
+    });
+    return data;
+}
+
+exports.readDbAll = readDbAll;
+
+const deleteDb = (db, title) => {
+    if (dbExiste(db, title) === false) {
+        alert(` ${title} não encontrado no banco de dados.`);
+        return;
+    }
+    var aRemover = fireBase.database().ref(`${db}/${title}/`);
+    aRemover.remove(function (error) {
+        alert(`${title} removido do banco de dados.`);
+    });
+}
+
+exports.deleteDb = deleteDb;  
